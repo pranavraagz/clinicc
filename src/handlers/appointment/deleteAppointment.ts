cedeff741ef9aa317d90d0a65a80cb0b6dfdeb6d
pranavraagz <pranavraagz@gmail.com>
2022-04-22 20:54:17 +0530

    feat: delete appointments

---
+++ src/handlers/appointment/deleteAppointment.ts
@@ -0,0 +1,30 @@
+import { Request, response, Response } from "express";
+import Joi from "joi";
+import { Appointment } from "../../entity/appointment";
+import { AppDataSource } from "../../service/data-source";
+
+export async function deleteAppointment(req: Request, res: Response) {
+  const schema = Joi.object({
+    id: Joi.number().required(),
+  });
+
+  const { value, error } = schema.validate(req.body);
+  if (error != null) {
+    console.log(error);
+    response.status(401).json({ error: error.message });
+  }
+
+  const { id } = value;
+
+  const repo = AppDataSource.manager.getRepository(Appointment);
+  const appointment = await repo.findOneBy({ id: parseInt(id) });
+
+  if (appointment == null) {
+    res.status(404).json({ msg: "Appointment does not exist" });
+    return;
+  }
+
+  repo.delete({ id: parseInt(id) });
+
+  res.sendStatus(200);
+}
--- src/routes/appointment.ts
+++ src/routes/appointment.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { createAppointment } from "../handlers/appointment/createAppointment";
+import { deleteAppointment } from "../handlers/appointment/deleteAppointment";
 
 export const appointmentRouter = Router();
 
 appointmentRouter.post("/", createAppointment);
+appointmentRouter.delete("/", deleteAppointment);
