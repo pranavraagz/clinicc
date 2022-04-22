d3fe336c92d14e1c2f3b5891946df85f04902ad9
pranavraagz <pranavraagz@gmail.com>
2022-04-22 20:40:20 +0530

    feat: search for patient by name

---
+++ src/handlers/patient/searchPatientByName.ts
@@ -0,0 +1,27 @@
+import { Request, response, Response } from "express";
+import Joi from "joi";
+import { Patient } from "../../entity/patient";
+import { AppDataSource } from "../../service/data-source";
+
+export async function searchPatientByName(req: Request, res: Response) {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+  });
+
+  const { value, error } = schema.validate(req.body);
+  if (error != null) {
+    console.log(error);
+    response.status(401).json({ error: error.message });
+  }
+
+  const { name } = value;
+
+  const result = await AppDataSource.manager
+    .getRepository(Patient)
+    .createQueryBuilder()
+    .select()
+    .where("name ILIKE :searchTerm", { searchTerm: `%${name}%` })
+    .getMany();
+
+  res.status(200).send(result);
+}
--- src/routes/patient.ts
+++ src/routes/patient.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { createPatient } from "../handlers/patient/createPatient";
 import { getPatient } from "../handlers/patient/getPatient";
+import { searchPatientByName } from "../handlers/patient/searchPatientByName";
 import { authenticateJWT } from "../middleware/auth";
 
 export const patientRouter = Router();
 
+patientRouter.post("/search", searchPatientByName);
 patientRouter.post("/", createPatient);
 patientRouter.get("/:id", getPatient);
