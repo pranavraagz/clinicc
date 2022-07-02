69f3d9333d27d440272913e79783d226eeeb291b
pranavraagz <pranavraagz@gmail.com>
2022-07-02 14:06:13 +0530

    feat: add fromtoday option to get row count appointment

--- src/handlers/appointment/getAppointmentRowCount.ts
+++ src/handlers/appointment/getAppointmentRowCount.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import Joi from "joi";
+import { Raw } from "typeorm";
 import { Appointment } from "../../entity/appointment";
 import { ac } from "../../service/access-control";
 import { AppDataSource } from "../../service/data-source";
@@ -10,7 +12,23 @@ export async function getAppointmentRowCount(req: Request, res: Response) {
     return res.sendStatus(403);
   }
 
-  const result = await AppDataSource.manager.getRepository(Appointment).count();
+  const { value, error } = Joi.object({
+    fromtoday: Joi.boolean().default(false),
+  }).validate(req.query);
+  if (error != null) {
+    return res.status(400).send(error);
+  }
+
+  const { fromtoday } = value;
+
+  let result: number;
+  if (fromtoday) {
+    result = await AppDataSource.manager.getRepository(Appointment).count({
+      where: { startTime: Raw((alias) => `${alias} >= CURRENT_DATE`) },
+    });
+  } else {
+    result = await AppDataSource.manager.getRepository(Appointment).count();
+  }
 
   res.status(200).json(result);
 }
