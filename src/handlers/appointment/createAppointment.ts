760b53c4e4093c81af47e726a5a0eb243e343ac5
pranavraagz <pranavraagz@gmail.com>
2022-07-06 14:22:20 +0530

    feat: add existing appointment check logic to avoid overlaps

--- src/handlers/appointment/createAppointment.ts
+++ src/handlers/appointment/createAppointment.ts
@@ -1,48 +1,69 @@
 import { add } from "date-fns";
 import { Request, Response } from "express";
 import Joi from "joi";
+import { LessThan, LessThanOrEqual, MoreThan, MoreThanOrEqual } from "typeorm";
 import { Appointment } from "../../entity/appointment";
 import { ac } from "../../service/access-control";
 import { AppDataSource } from "../../service/data-source";
 
 export async function createAppointment(req: Request, res: Response) {
   try {
+    // Permission check
     const permission = ac.can(req.user?.role).create("appointment");
     if (!permission.granted) {
       return res.sendStatus(403);
     }
+
+    // Input validation
     const schema = Joi.object({
       start: Joi.date().required(),
       end: Joi.date().optional(),
       doctor_id: Joi.number().required(),
       patient_id: Joi.number().required(),
     });
-
     const { value, error } = schema.validate(req.body);
     if (error != null) {
       console.error(error);
       return res.status(400).json({ error: error.message });
     }
-
     const { start, end, doctor_id, patient_id } = value;
-
-    const appointment = new Appointment();
-
-    const startTime = new Date(start);
-    let endTime: Date;
+    const inputStartTime = new Date(start);
+    let inputEndTime: Date;
     if (end != undefined) {
-      endTime = new Date(end);
+      inputEndTime = new Date(end);
     } else {
-      endTime = add(startTime, {
+      inputEndTime = add(inputStartTime, {
         minutes: Appointment.APPOINTMENT_DURATION_MINUTES,
       });
     }
 
-    appointment.startTime = startTime;
-    appointment.endTime = endTime;
+    // Checking for existing appointment:
+    const existing = await AppDataSource.getRepository(Appointment).findOne({
+      where: [
+        {
+          startTime: LessThanOrEqual(inputStartTime),
+          endTime: MoreThanOrEqual(inputEndTime),
+        },
+        {
+          endTime: MoreThan(inputStartTime),
+          startTime: LessThan(inputStartTime),
+        },
+        { startTime: LessThan(inputEndTime), endTime: MoreThan(inputEndTime) },
+        {},
+      ],
+    });
+    if (existing) {
+      return res.status(400).send("Appointment already exists in time period.");
+    }
+
+    // Creation of new appointment
+    const appointment = new Appointment();
+    appointment.startTime = inputStartTime;
+    appointment.endTime = inputEndTime;
     appointment.doctor = doctor_id;
     appointment.patient = patient_id;
 
+    // Add to database
     await AppDataSource.manager.save(appointment);
 
     res.sendStatus(201);
--- src/handlers/appointment/updateAppointment.ts
+++ src/handlers/appointment/updateAppointment.ts
@@ -1,57 +1,80 @@
 import { add } from "date-fns";
 import { Request, Response } from "express";
 import Joi from "joi";
+import { LessThan, LessThanOrEqual, MoreThan, MoreThanOrEqual } from "typeorm";
 import { Appointment } from "../../entity/appointment";
 import { ac } from "../../service/access-control";
 import { AppDataSource } from "../../service/data-source";
 import { logger } from "../../service/logger";
 
 export async function updateAppointment(req: Request, res: Response) {
-  const permission = ac.can(req.user?.role).update("appointment");
-  if (!permission.granted) {
-    return res.sendStatus(403);
-  }
-  const schema = Joi.object({
-    id: Joi.number().required(),
-    start: Joi.date().required(),
-    end: Joi.date().optional(),
-  });
+  try {
+    // Permission check
+    const permission = ac.can(req.user?.role).update("appointment");
+    if (!permission.granted) {
+      return res.sendStatus(403);
+    }
 
-  const { value, error } = schema.validate(req.body);
-  if (error != null) {
-    logger.warn(error);
-    return res.status(401).json({ error: error.message });
-  }
-
-  const { start, end, id } = value;
-
-  const appointment = new Appointment();
-
-  const startTime = new Date(start);
-  let endTime: Date;
-  if (end != undefined) {
-    endTime = new Date(end);
-  } else {
-    endTime = add(startTime, {
-      minutes: Appointment.APPOINTMENT_DURATION_MINUTES,
+    // Input validation
+    const schema = Joi.object({
+      id: Joi.number().required(),
+      start: Joi.date().required(),
+      end: Joi.date().optional(),
     });
+    const { value, error } = schema.validate(req.body);
+    if (error != null) {
+      logger.warn(error);
+      return res.status(401).json({ error: error.message });
+    }
+    const { start, end, id } = value;
+    const inputStartTime = new Date(start);
+    let inputEndTime: Date;
+    if (end != undefined) {
+      inputEndTime = new Date(end);
+    } else {
+      inputEndTime = add(inputStartTime, {
+        minutes: Appointment.APPOINTMENT_DURATION_MINUTES,
+      });
+    }
+
+    // Find existing appointment
+    const appointmentToUpdate = await AppDataSource.manager
+      .getRepository(Appointment)
+      .findOneBy({ id: id });
+
+    if (appointmentToUpdate == null) {
+      return res.status(404).send("Appointment not found");
+    }
+
+    // Checking for overlaps upon update:
+    const existingOverlap = await AppDataSource.getRepository(
+      Appointment
+    ).findOne({
+      where: [
+        {
+          startTime: LessThanOrEqual(inputStartTime),
+          endTime: MoreThanOrEqual(inputEndTime),
+        },
+        {
+          endTime: MoreThan(inputStartTime),
+          startTime: LessThan(inputStartTime),
+        },
+        { startTime: LessThan(inputEndTime), endTime: MoreThan(inputEndTime) },
+        {},
+      ],
+    });
+    if (existingOverlap) {
+      return res.status(400).send("Appointment already exists in time period.");
+    }
+
+    appointmentToUpdate.startTime =
+      inputStartTime ?? appointmentToUpdate.startTime;
+    appointmentToUpdate.endTime = inputEndTime;
+
+    await AppDataSource.manager.save(appointmentToUpdate);
+
+    res.sendStatus(200);
+  } catch (error) {
+    return res.status(500).send(error);
   }
-
-  appointment.startTime = startTime;
-  appointment.endTime = endTime;
-
-  const result = await AppDataSource.manager
-    .getRepository(Appointment)
-    .findOneBy({ id: id });
-
-  if (result == null) {
-    return res.status(404).json({ msg: "Appointment not found" });
-  }
-
-  result.startTime = startTime ?? result.startTime;
-  result.endTime = endTime;
-
-  await AppDataSource.manager.save(result);
-
-  res.sendStatus(200);
 }
