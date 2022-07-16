908c069ce76788a2e15c066f87cbe3e6b5e5735c
pranavraagz <pranavraagz@gmail.com>
2022-07-16 11:14:53 +0530

    feat: Create doctor upon new user creation when user is role doctor

---
+++ src/controllers/doctor/createDoctor.ts
@@ -0,0 +1,40 @@
+import { Doctor } from "../../entity/doctor";
+import { User } from "../../entity/user";
+import { AppDataSource } from "../../service/data-source";
+
+export async function createDoctor(
+  userId: number,
+  options?: { name?: string; phone?: string }
+) {
+  try {
+    const user = await AppDataSource.getRepository(User).findOne({
+      where: { id: userId },
+      relations: { doctor: true },
+    });
+
+    console.log(user);
+
+    if (user == null) {
+      throw new Error("User not found");
+    }
+    if (user.doctor) {
+      throw new Error("This user is already linked to a doctor account");
+    }
+    if (user.role !== "doctor") {
+      throw new Error(
+        "User does have have doctor role and therefore cannot be used to create a doctor"
+      );
+    }
+
+    const doctor = new Doctor();
+
+    doctor.name = options?.name ?? user.name;
+    doctor.phone = options?.phone ?? user.phone;
+    doctor.user = user;
+
+    await AppDataSource.getRepository(Doctor).save(doctor);
+    return;
+  } catch (error) {
+    throw error;
+  }
+}
--- src/handlers/user/createUser.ts
+++ src/handlers/user/createUser.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import Joi from "joi";
+import { createDoctor } from "../../controllers/doctor/createDoctor";
 import { User } from "../../entity/user";
 import { ac } from "../../service/access-control";
 import { AppDataSource } from "../../service/data-source";
@@ -28,18 +29,30 @@ export async function createUser(req: Request, res: Response) {
       return;
     }
     const { name, phone, password, role } = value;
-    const user = new User();
+    let user = new User();
     user.name = name;
     user.password = password;
     user.phone = phone;
     user.role = role;
 
     try {
-      await AppDataSource.manager.save(user);
+      user = await AppDataSource.manager.save(user);
     } catch (error) {
       res.status(500).send(error);
       return;
     }
+
+    // If user is a doctor, then create the corresponding doctor as well
+    if (user.role === "doctor") {
+      try {
+        await createDoctor(user.id);
+      } catch (error) {
+        // If Doctor creation fails, then delete the user and return the error
+        await AppDataSource.getRepository(User).delete({ id: user.id });
+        return res.status(400).send(error);
+      }
+    }
+
     res.sendStatus(201);
   } catch (error) {
     logger.error(error);
